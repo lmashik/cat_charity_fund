45cd72b530faa8a77ccca69726e18d21b7660065
Мария Лапикова <mashik_p@mail.ru>
2023-07-22 02:34:45 +0300

    added investment func to creating project process; moved extra checking to validators

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -1,16 +1,30 @@
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.validators import check_name_duplicate, check_project_exists
+from app.api.exceptions import DuplicateException
+from app.api.validators import (
+    check_invested_amount,
+    check_investing_funds,
+    check_name_duplicate,
+    check_project_exists,
+    check_project_open,
+)
 from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_project_crud
+from app.models import Donation
 from app.schemas.charity_project import (
     CharityProjectCreate,
     CharityProjectDB,
     CharityProjectUpdate,
+)
+from app.services.utils import (
+    close_item,
+    funds_distribution,
+    get_uninvested_objects,
 )
 
 router = APIRouter()
@@ -29,6 +43,16 @@ async def create_new_charity_project(
     """Создание проектов. Только для суперюзеров."""
     await check_name_duplicate(charity_project.name, session)
     new_project = await charity_project_crud.create(charity_project, session)
+    unallocated_donations = await get_uninvested_objects(Donation, session)
+    try:
+        funds_distribution(
+            opened_items=unallocated_donations, funds=new_project
+        )
+        await session.commit()
+        await session.refresh(new_project)
+    except IntegrityError:
+        await session.rollback()
+        raise DuplicateException('Средства уже распределены')
     return new_project
 
 
@@ -55,28 +79,21 @@ async def partially_update_charity_project(
 ):
     """Редактирование проектов. Только для суперюзеров."""
     charity_project = await check_project_exists(project_id, session)
-
-    if charity_project.close_date:
-        raise HTTPException(
-            status_code=400,
-            detail='Закрытый проект нельзя редактировать!'
-        )
+    await check_project_open(project_id, session)
 
     if obj_in.name:
         await check_name_duplicate(obj_in.name, session)
-
     if obj_in.full_amount:
-        if obj_in.full_amount < charity_project.invested_amount:
-            raise HTTPException(
-                status_code=400,
-                detail=(
-                    'Требуемая сумма проекта не может быть меньше вложенной!'
-                )
-            )
+        await check_investing_funds(project_id, obj_in.full_amount, session)
 
     charity_project = await charity_project_crud.update(
         charity_project, obj_in, session
     )
+    # на это вроде теста нет?..
+    if obj_in.full_amount == charity_project.invested_amount:
+        close_item(charity_project)
+        await session.commit()
+        await session.refresh(charity_project)
 
     return charity_project
 
@@ -92,11 +109,7 @@ async def remove_charity_project(
 ):
     """Удаление проектов. Только для суперюзеров."""
     charity_project = await check_project_exists(project_id, session)
-    if charity_project.invested_amount > 0:
-        raise HTTPException(
-            status_code=400,
-            detail='В проект были внесены средства, не подлежит удалению!'
-        )
+    await check_invested_amount(project_id, session)
     charity_project = await charity_project_crud.remove(
         charity_project, session
     )
