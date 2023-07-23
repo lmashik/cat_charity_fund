497789689df131630e34785bfd95a3cfb3e2c85c
Мария Лапикова <mashik_p@mail.ru>
2023-07-24 01:35:24 +0300

    logging config added and enabled; logs for user registration, start and close app added

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,10 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from typing import Optional
 
 from pydantic import BaseSettings, EmailStr
+
+from app.constants import BASE_DIR, DT_FORMAT, LOG_FORMAT
 
 
 class Settings(BaseSettings):
@@ -16,3 +20,18 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+def configure_logging():
+    log_dir = BASE_DIR / 'logs'
+    log_dir.mkdir(exist_ok=True)
+    log_file = log_dir / 'qrkot.log'
+    rotating_handler = RotatingFileHandler(
+        log_file, maxBytes=10 ** 6, backupCount=5
+    )
+    logging.basicConfig(
+        datefmt=DT_FORMAT,
+        format=LOG_FORMAT,
+        level=logging.INFO,
+        handlers=(rotating_handler, logging.StreamHandler())
+    )
